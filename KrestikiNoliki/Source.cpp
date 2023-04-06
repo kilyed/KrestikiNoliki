d8805e47fe9d89392befb6a4f87e8e0204945cae
kilyed <pon4ikig@gmail.com>
2023-04-06 11:39:59 +0300

    Add check and result Function

--- KrestikiNoliki/Source.cpp
+++ KrestikiNoliki/Source.cpp
@@ -20,6 +20,45 @@ void show_cells() {
     cout << "-" << cells[6] << "-" << '|' << "-" << cells[7] << "-" << '|' << "-" << cells[8] << "-" << "\n" << endl;
 
 }
+void make_move(int num) {
+    if (num == 1) cout << PlayerName1;
+    else cout << PlayerName2;
+    int cell;
+    cout << ",введите номер €чейки,сделайте свой ход:";
+    cin >> cell;
+   
+
+       
+    while (cell > 9 || cell < 1 || cells[cell - 1] == 'O' || cells[cell - 1] == 'X')
+        cout << "¬ведите номер правильного ( 1-9 ) или пустой ( --- ) клетки , чтобы сделать ход:";
+    cin >> cell;
+    cout << "\n";
+   
+    if (num == 1) cells[cell - 1] = 'X';
+    else cells[cell - 1] = 'O';
+}
+
+
+
+char check()
+{
+    for (int i = 0; i < 3; i++) 
+        if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) 
+            return cells[i]; 
+   
+        else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) 
+            return cells[i];
+        else if ((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]))
+            return cells[i]; 
+    return '-'; 
+}
+
+void result() {
+    if (win == 'X')
+        cout << PlayerName1 << "You Win! " << PlayerName2 << " or you lose..." << endl;
+    else if (win == 'O') cout << PlayerName2 << "You Win!  " << PlayerName1 << " or you lose..." << endl;
+
+}
 void main()
 {
     show_cells();
