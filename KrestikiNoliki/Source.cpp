4a7c8117bace1c1654334b8c10c5107034c47c6e
kilyed <pon4ikig@gmail.com>
2023-04-06 11:32:44 +0300

    Add cell and mass for cell

--- KrestikiNoliki/Source.cpp
+++ KrestikiNoliki/Source.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+char win = '-'; 
+char PlayerName1[80], PlayerName2[80]; 
+char cells[9] = { '-','-','-','-','-','-','-','-','-' };
+
+void show_cells() {
+    system("cls");
+
+   
+    cout << "Number cell: \n";
+    cout << "-" << 1 << "-" << "|" << "-" << 2 << "-" << "|" << "-" << 3 << "-" << endl;
+    cout << "-" << 4 << "-" << "|" << "-" << 5 << "-" << "|" << "-" << 6 << "-" << endl;
+    cout << "-" << 7 << "-" << "|" << "-" << 8 << "-" << "|" << "-" << 9 << "-" << endl;
+
+    cout << "Cell  now (--- = empty):\n" << endl;
+    cout << "-" << cells[0] << "-" << '|' << "-" << cells[1] << "-" << '|' << "-" << cells[2] << "-" << endl;
+    cout << "-" << cells[3] << "-" << '|' << "-" << cells[4] << "-" << '|' << "-" << cells[5] << "-" << endl;
+    cout << "-" << cells[6] << "-" << '|' << "-" << cells[7] << "-" << '|' << "-" << cells[8] << "-" << "\n" << endl;
+
+}
 void main()
 {
-
+    show_cells();
 }
